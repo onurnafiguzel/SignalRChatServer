ab12cdbdf8e747dff65fce9bfa3170f4e7cc522d
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-07-24 19:31:56 +0300

    CORS politikasının ayarlanması

---
+++ SignalRChatServer/Hubs/ChatHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+    }
+}
--- SignalRChatServer/Program.cs
+++ SignalRChatServer/Program.cs
@@ -1,6 +1,19 @@
+using SignalRChatServer.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+options.AddDefaultPolicy(policy =>
+    policy.AllowCredentials()
+          .AllowAnyHeader()
+          .AllowAnyMethod()
+          .SetIsOriginAllowed(x => true)));
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
+app.UseCors();
 app.MapGet("/", () => "Hello World!");
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
